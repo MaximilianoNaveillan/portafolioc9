84727ca9a9925fc2ef668c8816d6d9fa32c8c953
MaximilianoNaveillan <maximilianonaveillan@gmail.com>
2024-02-09 14:26:54 -0300

    first commit

---
+++ proyectos.js
@@ -0,0 +1,20 @@
+const proyectosData = [
+    {
+        nombre: "Proyecto 1",
+        descripcion: "Descripción del Proyecto 1.",
+        enlaceGithub: "https://github.com/tu-usuario/proyecto1",
+        imagen: "assets/proyectos/proyecto-0.jpg",
+    },
+    {
+        nombre: "Proyecto 2",
+        descripcion: "Descripción del Proyecto 2.",
+        enlaceGithub: "https://github.com/tu-usuario/proyecto2",
+        imagen: "assets/proyectos/proyecto-1.jpg",
+    },
+    {
+        nombre: "Proyecto 3",
+        descripcion: "Descripción del Proyecto 3.",
+        enlaceGithub: "https://github.com/tu-usuario/proyecto2",
+        imagen: "assets/proyectos/proyecto-1.jpg",
+    },
+];
